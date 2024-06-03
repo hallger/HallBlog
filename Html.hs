fd65c052d3814cb75be77e58c3f75120e198102b
hallger <hall@hallgerd.com>
2024-06-03 10:25:59 -0400

    added html constructor and changed tag_ to tag

--- Html.hs
+++ Html.hs
@@ -1,28 +1,43 @@
+import Control.Monad.RWS (gets)
 -- Html.hs
 
 -- * Types
 newtype Structure = Structure String
+newtype Html = Html String
+type Title = String
+type Body = String
 
 -- * Tags
+
+html_ :: Title -> Structure -> Html
+html_ title content =
+  Html
+  ( tag "html"
+    (tag "head" (tag "title" (escape title))
+      <> tag "body" (getStructStr content)
+    )
+  )
+
+
 p_ :: String -> Structure
-p_ = Structure . tag_ "p"
+p_ = Structure . tag "p"
 
 h1_ :: String -> Structure
-h1_ = Structure . tag_ "h1"
+h1_ = Structure . tag "h1"
 
 h2_ :: String -> Structure
-h2_ = Structure . tag_ "h2"
+h2_ = Structure . tag "h2"
 
 list_ :: String -> [Structure] -> Structure
 list_ list_type
   | list_type == "ul" = 
-    Structure . tag_ "ul" . concat . map (tag_ "li" . getStructStr)
+    Structure . tag "ul" . concat . map (tag "li" . getStructStr)
   | list_type == "ol" =
-    Structure . tag_ "ul" . concat . map (tag_ "li" . getStructStr)
+    Structure . tag "ul" . concat . map (tag "li" . getStructStr)
 
 -- * Utils
-tag_ :: String -> String -> String
-tag_ t c
+tag :: String -> String -> String
+tag t c
   = "<" <> t <> ">" <> c <> "</" <> t <> ">"
 
 escape :: String -> String
