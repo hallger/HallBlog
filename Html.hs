3d073f6c4d39df1ee0292c6fe8349a6b9c9356c1
hallger <hall@hallgerd.com>
2024-06-03 09:34:41 -0400

    added paragraph tag to html

--- Html.hs
+++ Html.hs
@@ -2,9 +2,12 @@
 
 newtype Structure = Structure String
 
+p_ :: String -> Structure
+p_ = Structure . el "p"
+
 h1_ :: String -> Structure
-h1_ = Structure . tag "h1"
+h1_ = Structure . tag_ "h1"
 
 tag_ :: String -> String -> String
-tag t c
+tag_ t c
   = "<" <> t <> ">" <> c <> "</" <> t <> ">"
