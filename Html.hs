3811b4a5cf17d225b5bba0433df9be2d35850203
hallger <hall@hallgerd.com>
2024-06-03 09:36:31 -0400

    added escape function for Htmls

--- Html.hs
+++ Html.hs
@@ -3,7 +3,7 @@
 newtype Structure = Structure String
 
 p_ :: String -> Structure
-p_ = Structure . el "p"
+p_ = Structure . tag_ "p"
 
 h1_ :: String -> Structure
 h1_ = Structure . tag_ "h1"
@@ -11,3 +11,17 @@ h1_ = Structure . tag_ "h1"
 tag_ :: String -> String -> String
 tag_ t c
   = "<" <> t <> ">" <> c <> "</" <> t <> ">"
+
+escape :: String -> String
+escape =
+  let
+    escapeChar c =
+      case c of
+        '<' -> "&lt;"
+        '>' -> "&gt;"
+        '&' -> "&amp;"
+        '"' -> "&quot;"
+        '\'' -> "&#39;"
+        '_' -> "[c]"
+    in
+        concat . map escapeChar
